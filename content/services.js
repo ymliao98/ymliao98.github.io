1cf76555adb3577045cba78b3baf57b35b8952de
CBlogx <958809247@qq.com>
2024-12-14 15:35:05 +0800

    update: add academic service

--- content/services.js
+++ content/services.js
@@ -1,15 +1,18 @@
 const services = [
   {
     type: "Journal Reviewer",
-    name:"IEEE Transactions on Mobile Computing(TMC)",
-    
-  },{
+    name: "IEEE Transactions on Mobile Computing(TMC)",
+  },
+  {
     type: "Journal Reviewer",
-    name:"IEEE Transactions on Parallel and Distributed Systems(TPDS)",
-    
-  },{
+    name: "IEEE Transactions on Parallel and Distributed Systems(TPDS)",
+  },
+  {
     type: "Journal Reviewer",
-    name:"IEEE/ACM Transactions on Networking(ToN)",
-    
+    name: "IEEE/ACM Transactions on Networking(ToN)",
+  },
+  {
+    type: "Journal Reviewer",
+    name: "IEEE Transactions on Communications(TCOM)",
   },
 ];
